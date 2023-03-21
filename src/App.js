fea34850718924aeb21cb8e5bb37dc22c73bbf7f
enrique aranaga <earanaga.fc@gmail.com>
2023-03-21 00:16:40 -0400

    cambios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import FreeCodeCampLogo from './imagenes/FreeCodeCamp_logo.png';
+import Boton from './componentes/Botones';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          src={FreeCodeCampLogo} 
+          className='freecodecamp-logo' 
+          alt="logo de freecodecamp" />
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'> 
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='fila'> </div>
+        <div className='fila'> </div>
+        <div className='fila'> </div>
+        <div className='fila'> </div>
+      </div>
     </div>
   );
 }
